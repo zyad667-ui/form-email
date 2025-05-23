d3bc65ff26e479632cae30b095e61fdb705b1760
zyad667-ui <ziadfiach4@gmail.com>
2025-05-23 10:16:10 +0100

    mail form

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import form from './partials/form'
+
+
+
+const HomePage = () => {
+
+    return (
+        <>
+<h1>helo world</h1>
+            
+
+        </>
+    );
+};
+
+export default HomePage;
